ebd03d42d93cfd35d871c196f241178ef67ad46b
Martyna Gawin <martynagawin1@gmail.com>
2023-03-16 12:08:13 +0100

    Extract widgets to other files

--- lib/features/weather_page/pages/weather_page.dart
+++ lib/features/weather_page/pages/weather_page.dart
@@ -1,5 +1,6 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:my_weather_app/widgets/top_widget.dart';
+import 'package:my_weather_app/widgets/widget_details.dart';
 
 class WeatherPage extends StatelessWidget {
   const WeatherPage({
@@ -20,108 +21,9 @@ class WeatherPage extends StatelessWidget {
               opacity: 0.7),
         ),
         child: Column(
-          children: [
-            Container(
-              width: double.infinity,
-              margin: const EdgeInsets.symmetric(
-                horizontal: 32,
-                vertical: 16,
-              ),
-              padding: const EdgeInsets.all(
-                32,
-              ),
-              decoration: BoxDecoration(
-                borderRadius: const BorderRadius.all(Radius.circular(32)),
-                gradient: const LinearGradient(
-                  begin: Alignment.topLeft,
-                  end: Alignment.bottomRight,
-                  colors: <Color>[
-                    Color.fromARGB(255, 243, 245, 236),
-                    Color.fromARGB(255, 187, 187, 180),
-                    Color.fromARGB(255, 184, 189, 181),
-                    Color.fromARGB(255, 114, 116, 108),
-                  ],
-                  tileMode: TileMode.mirror,
-                ),
-                boxShadow: [
-                  BoxShadow(
-                    color: Colors.grey.withOpacity(0.5),
-                    spreadRadius: 5,
-                    blurRadius: 7,
-                    offset: const Offset(0, 3),
-                  ),
-                ],
-              ),
-              child: Column(
-                children: [
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: const [
-                      Text(
-                        'Wrocław',
-                        style: TextStyle(
-                            fontSize: 20,
-                            fontWeight: FontWeight.bold,
-                            color: Colors.white),
-                      ),
-                      Text(
-                        '10°',
-                        style: TextStyle(
-                            fontSize: 20,
-                            fontWeight: FontWeight.bold,
-                            color: Colors.white),
-                      ),
-                    ],
-                  ),
-                ],
-              ),
-            ),
-            Container(
-              width: double.infinity,
-              margin: const EdgeInsets.symmetric(horizontal: 32, vertical: 16),
-              padding: const EdgeInsets.all(32),
-              decoration: BoxDecoration(
-                borderRadius: const BorderRadius.all(Radius.circular(32)),
-                gradient: const LinearGradient(
-                  begin: Alignment.topLeft,
-                  end: Alignment.bottomRight,
-                  colors: <Color>[
-                    Color.fromARGB(255, 243, 245, 236),
-                    Color.fromARGB(255, 187, 187, 180),
-                    Color.fromARGB(255, 184, 189, 181),
-                    Color.fromARGB(255, 114, 116, 108),
-                  ],
-                  tileMode: TileMode.mirror,
-                ),
-                boxShadow: [
-                  BoxShadow(
-                    color: Colors.grey.withOpacity(0.5),
-                    spreadRadius: 5,
-                    blurRadius: 7,
-                    offset: const Offset(0, 3),
-                  ),
-                ],
-              ),
-              child: Column(
-                children: [
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        'Szczegóły pogodowe'.toUpperCase(),
-                        style: const TextStyle(
-                            fontSize: 12,
-                            fontWeight: FontWeight.bold,
-                            color: Color.fromARGB(255, 9, 9, 9)),
-                      ),
-                      const Icon(Icons.refresh, color: Colors.grey, size: 24),
-                    ],
-                  ),
-                  const SizedBox(height: 10),
-                ],
-              ),
-            ),
+          children: const [
+            TopWidget(),
+            WeatherDetails(),
           ],
         ),
       ),
---
+++ lib/widgets/widget_details.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class WeatherDetails extends StatelessWidget {
+  const WeatherDetails({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      margin: const EdgeInsets.symmetric(horizontal: 32, vertical: 16),
+      padding: const EdgeInsets.all(32),
+      decoration: BoxDecoration(
+        borderRadius: const BorderRadius.all(Radius.circular(32)),
+        gradient: const LinearGradient(
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+          colors: <Color>[
+            Color.fromARGB(255, 243, 245, 236),
+            Color.fromARGB(255, 187, 187, 180),
+            Color.fromARGB(255, 184, 189, 181),
+            Color.fromARGB(255, 114, 116, 108),
+          ],
+          tileMode: TileMode.mirror,
+        ),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.grey.withOpacity(0.5),
+            spreadRadius: 5,
+            blurRadius: 7,
+            offset: const Offset(0, 3),
+          ),
+        ],
+      ),
+      child: Column(
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                'Szczegóły pogodowe'.toUpperCase(),
+                style: const TextStyle(
+                    fontSize: 12,
+                    fontWeight: FontWeight.bold,
+                    color: Color.fromARGB(255, 9, 9, 9)),
+              ),
+              const Icon(Icons.refresh, color: Colors.grey, size: 24),
+            ],
+          ),
+          const SizedBox(height: 10),
+        ],
+      ),
+    );
+  }
+}
